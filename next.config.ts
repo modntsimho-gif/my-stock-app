dc87a2b2e71b0029edfa0be8b3bd1124969bdc14
WatchDogMaster <modntsimho@gmail.com>
2026-02-23 13:12:10 +0900

    path 설정 변경

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '/api/analyze',
-        destination: '/api/index.py',
+        source: '/api/:path*', // 모든 /api/로 시작하는 요청을
+        destination: '/api/index.py', // 파이썬 파일로 보냅니다.
       },
     ];
   },
